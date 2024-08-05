78f0071443a86013adb443834048ce7cec935674
Bonekazz <juracirdantas@gmail.com>
2024-08-04 23:57:24 -0300

    yaba

--- pages/home/index.ts
+++ pages/home/index.ts
@@ -1,14 +1,5 @@
-import { createIcons, X, Bell, BookOpenText, House, MapPinned } from 'lucide';
-import BoneModal from "../../../lib/Modals";
+import BoneModal from "../../lib/Modals";
 
-createIcons({
-  icons: {
-      BookOpenText,
-      House,
-      MapPinned,
-      Bell, X
-  }
-});
 
 const openHelpBtn = document.getElementById("help-btn") as HTMLButtonElement;
 const closeHelpBtn = document.querySelectorAll<HTMLElement>(".close-help-modal") as NodeListOf<HTMLElement>;
---
+++ pages/icons.ts
@@ -0,0 +1,10 @@
+import { createIcons, ChevronLeft, X, Bell, BookOpenText, House, MapPinned } from 'lucide';
+
+createIcons({
+  icons: {
+      BookOpenText,
+      House,
+      MapPinned,
+      Bell, X, ChevronLeft
+  }
+});
