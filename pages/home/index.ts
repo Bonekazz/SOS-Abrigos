724ebf63b82037e84b421565633e75437540843c
Bonekazz <juracirdantas@gmail.com>
2024-08-05 00:16:53 -0300

    aaaaaaa

--- pages/home/index.ts
+++
@@ -1,8 +0,0 @@
-import BoneModal from "../../lib/Modals";
-
-
-const openHelpBtn = document.getElementById("help-btn") as HTMLButtonElement;
-const closeHelpBtn = document.querySelectorAll<HTMLElement>(".close-help-modal") as NodeListOf<HTMLElement>;
-const helpModalDiv = document.getElementById("help-modal-div") as HTMLElement;
-
-new BoneModal({wrapperElement: helpModalDiv, openTriggers: [openHelpBtn], closeTriggers: closeHelpBtn});
